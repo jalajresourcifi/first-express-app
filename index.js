67320ee395c1c5c35bd6eb71aca00bedfe52e4e6
Jalaj Sharma <Jalaj.sharma@resourcifi.com>
2019-10-17 00:36:26 -0700

    [static serving] - served static css files and managed unhandled routes

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const postRoute = require('./routes/posts')
 
-const app = express()
+const app = express();
 
-app.use(function (req, res, next) {
-  console.log(2+2);
-  console.log('postRoute')
-  console.log(postRoute)
-	next();
-})
-
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(postRoute);
 
+app.use('/', (req, res, next) => {
+	res.sendFile(path.join(__dirname, 'views', '404.html'));
+})
 
 app.listen(3000, () => {
 	console.log('Server started on port 3000')
--- routes/posts.js
+++ routes/posts.js
@@ -3,12 +3,20 @@ const path = require('path');
 
 const router = express.Router()
 
+// Products Routes
 router.get('/', (req, res) => {
-	res.send('I am in get route')
+	console.log('inside get route');
+	res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 })
 
+router.get('/shop', (req, res) => {
+	res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+})
+
+
+// Post Routes
 router.get('/posts', (req, res) => {
-	res.sendFile(path.join(__dirname, '../', 'views', 'post.html'))
+	res.sendFile(path.join(__dirname, '../', 'views', 'post.html'));
 })
 
 router.post('/posts', (req, res) => {
